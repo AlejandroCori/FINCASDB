e3a0e614f8e2e1944295d709cf89ab882fda1d36
AlejandroCori <corifloresangelalejandro@gmail.com>
2024-06-16 17:40:52 -0400

    Se cambiaron las tablas

---
+++ conexion.php
@@ -0,0 +1,18 @@
+<?php
+   // Dirección o IP del servidor MySQL
+   $host = "localhost"; //"127.0.0.1"
+   // Puerto del servidor MySQL
+   $puerto = 3306; // Debe ser un entero, no una cadena
+   // Nombre de usuario del servidor MySQL
+   $usuario = "root";
+   // Contraseña del usuario
+   $contrasena = "";
+   // Nombre de la base de datos
+   $baseDeDatos ="fincas";
+
+   $Conexion = new  mysqli($host, $usuario, $contrasena, $baseDeDatos, $puerto);
+   
+   if (!$Conexion) {
+       echo "Fallo al conectar a MySQL";
+   }
+?>
